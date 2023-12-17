68f654243671e3081aadbb312100acb5991c3164
birol-coskun <coskun.birol@yahoo.com>
2023-12-18 00:45:07 +0300

    dark mode

--- README.md
+++ README.md
@@ -19,4 +19,3 @@ git clone https://github.com/birol-coskun/doto.git
 Navigate to the project directory:
 
 cd doto
-
---
+++ static/dark-mode-toggle.js
@@ -0,0 +1,7 @@
+// dark-mode-toggle.js
+
+// Toggle dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
