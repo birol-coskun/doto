cfc021b7c456749f95df567f384267c2a53a4c44
birol-coskun <coskun.birol@yahoo.com>
2023-12-17 22:30:22 +0300

    Task ekleme, düzenleme, silme işlemleri; zamanı gösterme; renkli tamamlanmış görevler; düğme düzenlemeleri

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ import pytz
 app = Flask(__name__)
 
 class Task:
-    def __init__(self, id, content, date_created):
+    def __init__(self, id, content, date_created, completed=False):
         self.id = id
         self.content = content
         self.date_created = date_created
         self.last_updated = None
+        self.completed = completed
 
 tasks = [
     Task(1, "Task 1", datetime(2023, 12, 17, 17, 0, 0, tzinfo=timezone.utc)),
+    # Diğer görevler...
 ]
 
 @app.route('/')
 def index():
     for task in tasks:
-        task.date_local = task.date_created.astimezone(timezone(timedelta(hours=3)))
+        task.date_local = task.date_created.astimezone(timezone(timedelta(hours=3)))  # GMT+3 için
         task.last_updated_local = task.last_updated.astimezone(timezone(timedelta(hours=3))) if task.last_updated else None
 
     return render_template('index.html', tasks=tasks)
@@ -36,32 +38,31 @@ def add():
 @app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
 def edit(task_id):
     task = next((task for task in tasks if task.id == task_id), None)
-    if task:
+    if task and not task.completed:
         if request.method == 'POST':
             task.content = request.form.get('content')
             task.last_updated = datetime.utcnow().replace(tzinfo=pytz.utc)
             return redirect(url_for('index'))
         else:
+            task.date_local = task.date_created.astimezone(timezone(timedelta(hours=3)))  # GMT+3 için
             return render_template('edit.html', task=task)
     else:
-        return 'Task not found', 404
-
-# 'update' endpoint'i ekleniyor
-@app.route('/update/<int:task_id>', methods=['POST'])
-def update(task_id):
-    task = next((task for task in tasks if task.id == task_id), None)
-    if task:
-        task.content = request.form.get('content')
-        task.last_updated = datetime.utcnow().replace(tzinfo=pytz.utc)
-        return redirect(url_for('index'))
-    else:
-        return 'Task not found', 404
+        return 'Task not found or completed', 404
 
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
     global tasks
     tasks = [task for task in tasks if task.id != task_id]
     return redirect(url_for('index'))
+
+@app.route('/complete/<int:task_id>')
+def complete(task_id):
+    task = next((task for task in tasks if task.id == task_id), None)
+    if task:
+        task.completed = not task.completed
+        return redirect(url_for('index'))
+    else:
+        return 'Task not found', 404
 
 if __name__ == '__main__':
     app.run(debug=True)
